ed611092de57f0956c90309c7db0fb413c8c0c8c
Kentaro Murayama <r51152@hotmail.co.jp>
2019-10-23 20:22:13 +0900

    first commit

---
+++ verilogmoduleio/verilogmoduleio/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace verilogmoduleio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // read verilog file
+            using (StreamReader fr = new StreamReader(args[0]))
+            {
+                var code = fr.ReadToEnd();
+                verilogparse vp = new verilogparse();
+
+                var result = vp.commentRemove(code);
+
+                Console.WriteLine(result);
+
+            }
+
+        }
+    }
+}
---
+++ verilogmoduleio/verilogmoduleio/verilogparse.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace verilogmoduleio
+{
+    class verilogparse
+    {
+        public readonly string linecommentword = "//";
+        public readonly string startareacommentword = "/*";
+        public readonly string endareacommentword = "*/";
+        public string commentRemove(string verilogcode)
+        {
+            bool linecomment = false;
+            bool areacomment = false;
+            string removedcode = string.Empty;
+
+            var reg = new Regex(@"\*[\s\S]*?\*/|//.*");
+
+            removedcode = reg.Replace(verilogcode, " ");
+
+            return removedcode;
+
+
+        }
+/*
+        private (string, bool, bool) linecommentRemove(string linecode, bool currentlinecomment, bool currentareacomment)
+        {
+            bool linecomment = false;
+            bool areacomment = false;
+            string removedcode = linecode;
+
+            // find '//'
+            var linecommentposition = removedcode.IndexOf(linecommentword);
+            var startareacommentposition = removedcode.IndexOf(startareacommentword);
+            var endareacommentposition = removedcode.IndexOf(endareacommentword);
+
+            if ( currentareacomment == true)
+            {
+                if (endareacommentposition >= 0)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+
+
+
+            }
+
+
+
+
+
+        }
+        */
+
+        // /*da
+        /* //afdsa
+         */
+
+        
+
+
+    }
+}
